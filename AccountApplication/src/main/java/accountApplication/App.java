0d98377803bbf6a936d89343f3021e5a349437f5
DarylMcbride <Admin@STUDENT13>
2019-04-15 14:18:18 +0100

    initial commit

---
+++ AccountApplication/src/main/java/accountApplication/App.java
@@ -0,0 +1,13 @@
+package accountApplication;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World" );
+    }
+}
